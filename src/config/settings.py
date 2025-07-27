ec91c21844c0f53ab6c931f7b5a212aad775b3a8
7e_nv <pycartem@mail.ru>
2025-07-27 16:52:19 +1000

    upd

--- src/config/settings.py
+++ src/config/settings.py
@@ -10,4 +10,5 @@ class settings:
         self.DB_USER = getenv("DB_USER")
         self.DB_PASS = getenv("DB_PASS")
         self.DB_NAME = getenv("DB_NAME")
-        self.DATABASE_URL_asyncpg = f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        #self.DATABASE_URL_asyncpg = f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        self.DATABASE_URL_asyncpg = "postgresql+asyncpg://sw_test_user:ZACZq7yfm7frt9GTg6hHjUttjSkk5UKG@dpg-d22rl1e3jp1c7397ogt0-a.oregon-postgres.render.com/sw_test"
